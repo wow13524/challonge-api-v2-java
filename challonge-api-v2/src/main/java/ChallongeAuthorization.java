8ef01b92494b06a312dc93ba8a693e3310cce2f9
wow13524 <50389483+wow13524@users.noreply.github.com>
2023-01-14 23:57:14 -0800

    Work on authenticating client

--- challonge-api-v2/src/main/java/ChallongeAuthorization.java
+++ challonge-api-v2/src/main/java/ChallongeAuthorization.java
@@ -1,26 +1,45 @@
 package main.java;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+
 public final class ChallongeAuthorization {
-    private String clientId, clientSecret, refreshToken;
+    private final String clientId, clientSecret, redirectUri;
+    private String refreshToken;
+    private FileReader reader;
+    private FileWriter writer;
 
-    public ChallongeAuthorization(String clientId, String clientSecret, String refreshToken) {
-        assert clientId != null : "clientId is null";
-        assert clientSecret != null : "clientSecret is null";
-        assert refreshToken != null : "refreshToken is null";
-        this.clientId = clientId;
-        this.clientSecret = clientSecret;
-        this.refreshToken = refreshToken;
-    }
+    public ChallongeAuthorization(File authFile) {
+        try {
+            this.reader = new FileReader(authFile);
+            this.writer = new FileWriter(authFile);
+        }
+        catch (FileNotFoundException e) {
+            //TODO error handling for FileNotFoundException
+        }
+        catch (IOException e) {
+            //TODO error handling for IOException
+        }
 
-    public String getClientId() {
-        return this.clientId;
-    }
-    
-    public String getClientSecret() {
-        return this.clientSecret;
-    }
-    
-    public String getRefreshToken() {
-        return this.refreshToken;
+        JSONObject data = (JSONObject)JSONValue.parse(reader);
+        Object clientId = data.get("client_id");
+        Object clientSecret = data.get("client_secret");
+        Object redirectUri = data.get("redirect_uri");
+        Object refreshToken = data.get("refresh_token");
+        assert clientId instanceof String : "client_id must be a String";
+        assert clientSecret instanceof String : "client_secret must be a String";
+        assert redirectUri instanceof String : "redirect_uri must be a String";
+        assert refreshToken instanceof String : "refresh_token must be a String";
+
+        this.clientId = (String)clientId;
+        this.clientSecret = (String)clientSecret;
+        this.redirectUri = (String)redirectUri;
+        this.refreshToken = (String)refreshToken;
     }
 }
--- challonge-api-v2/src/main/java/ChallongeClient.java
+++ challonge-api-v2/src/main/java/ChallongeClient.java
@@ -1,16 +1,50 @@
 package main.java;
 
-import java.net.http.*;
-import java.net.http.HttpClient.Version;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.HashMap;
+import org.json.simple.*;
+import org.json.simple.parser.JSONParser;
 
 public class ChallongeClient {
     private static final String GRANT_REDIRECT_URI = "";
     private static final String GRANT_REQUEST_URI = "https://api.challonge.com/oauth/authorize?scope=%s&client_id=%s&redirect_uri=%s&response_type=code";
 
+    private ChallongeAuthorization auth;
     private HttpClient client;
     private String accessToken;
     
     public ChallongeClient(ChallongeAuthorization auth) {
+        this.auth = auth;
         this.client = HttpClient.newHttpClient();
+        this.accessToken = getNewAccessToken();
+    }
+
+    private String getNewAccessToken() {
+        HashMap<String, String> body = new HashMap<String, String>();
+        body.put("refresh_token", this.auth.refreshToken);
+        body.put("client_id", this.auth.clientId);
+        body.put("grant_type", "refresh_token");
+        body.put("redirect_uri", this.auth.redirect_uri);
+
+        HttpRequest request = HttpRequest.newBuilder()
+        .uri(URI.create("https://api.challonge.com/oauth/token"))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .POST(HttpRequest.BodyPublishers.ofString(EncodeUtils.encodeFormBody(body)))
+        .build();
+
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            JSONObject parsed = (JSONObject)JSONValue.parse(response.body());
+            System.out.println(body.toString());
+            System.out.println(response.body());
+            System.out.println(parsed.toJSONString());
+        }
+        catch(Exception e) {
+            System.out.println("Exception! " + e);
+        }
+        return "";
     }
 }
