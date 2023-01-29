9983369db2f63e19e4599f66fffe9ac11f5c02a7
wow13524 <50389483+wow13524@users.noreply.github.com>
2023-01-28 23:59:16 -0800

    Cleaned up ChallongeAuthorization
    Rather than having ChallongeAuthorization extend AbstractSet and deal with having to create or modify Maps for saving a new token or creating the request body to request for a new token, there is now a simple ImmutableMap class that is used to expose both a public requestRequestBody map and house a private fileSaveBody.  Since they're both ImmutableMaps constructed with references to the same Entry objects, calling refresh_token.setValue() reflects in both!

---
+++ challonge-api-v2/src/main/java/ImmutableMap.java
@@ -0,0 +1,19 @@
+package main.java;
+
+import java.util.AbstractMap;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public final class ImmutableMap<T, U> extends AbstractMap<T, U> {
+    private final Set<Entry<T, U>> entrySet;
+
+    @SafeVarargs
+    public ImmutableMap(Entry<T, U>... entries) {
+        this.entrySet = new HashSet<Entry<T, U>>(Arrays.asList(entries));
+    }
+
+    public Set<Entry<T, U>> entrySet() {
+        return this.entrySet;
+    }
+}
