3a666dd0fa2db42ed53453bc1b6a627f966b6086
wow13524 <50389483+wow13524@users.noreply.github.com>
2023-01-31 23:48:29 -0800

    Implement base class equals/toString for debugging

--- challonge-api-v2/src/main/java/ChallongeObject.java
+++ challonge-api-v2/src/main/java/ChallongeObject.java
@@ -6,4 +6,22 @@ public class ChallongeObject {
         this.id = id;
         this.type = type;
     }
+
+    @Override
+    public boolean equals(Object other) {
+        if (!(other instanceof ChallongeObject)) {
+            return false;
+        }
+        ChallongeObject o = (ChallongeObject)other;
+        return this.id.equals(o.id) && this.type.equals(o.type);
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+            "ChallongeObject[id=%s, type=%s]",
+            this.id,
+            this.type
+        );
+    }
 }
