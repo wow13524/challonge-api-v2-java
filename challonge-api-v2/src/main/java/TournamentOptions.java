a9737267a1b97d83d95655f596787115a95bf734
wow13524 <50389483+wow13524@users.noreply.github.com>
2023-02-04 22:47:52 -0800

    Possible layout for tournament type options

---
+++ challonge-api-v2/src/main/java/TournamentOptions.java
@@ -0,0 +1,19 @@
+package main.java;
+
+public final class TournamentOptions {
+    public final class DoubleEliminationOptions {
+        
+    }
+
+    public final class RoundRobinOptions {
+
+    }
+
+    public final class SwissOptions {
+
+    }
+
+    public final class FreeForAllOptions {
+
+    }
+}
