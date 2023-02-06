5c802b5a6511106caa48ecb112fdb20cda1088d6
wow13524 <50389483+wow13524@users.noreply.github.com>
2023-02-05 23:07:39 -0800

    Trying one way to format TournamentOptions

--- challonge-api-v2/src/main/java/TournamentOptions.java
+++ challonge-api-v2/src/main/java/TournamentOptions.java
@@ -1,8 +1,38 @@
 package main.java;
 
+import main.java.Exceptions.ChallongeException;
+
 public final class TournamentOptions {
     public final class DoubleEliminationOptions {
-        
+        public enum GrandFinalsModifier {
+            NONE(""),
+            SKIP("skip"),
+            SINGLE_MATCH("single match");
+
+            public final String name;
+
+            GrandFinalsModifier(String name) {
+                this.name = name;
+            }
+
+            @Override
+            public String toString() {
+                return this.name;
+            }
+        }
+
+        private final boolean hideSeeds;
+        private final GrandFinalsModifier grandFinalsModifier;
+
+        public DoubleEliminationOptions(boolean hideSeeds, GrandFinalsModifier grandFinalsModifier) throws ChallongeException {
+            TypeUtils.requireType(
+                grandFinalsModifier,
+                GrandFinalsModifier.class,
+                "grandFinalsModifier"
+            );
+            this.hideSeeds = hideSeeds;
+            this.grandFinalsModifier = grandFinalsModifier;
+        }
     }
 
     public final class RoundRobinOptions {
