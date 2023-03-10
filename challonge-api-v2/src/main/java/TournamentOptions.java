a4dbad10917bbf2fc1f26005b4e100661b0c9312
wow13524 <50389483+wow13524@users.noreply.github.com>
2023-03-09 21:59:05 -0800

    Implemented .equals() for TournamentOptions

--- challonge-api-v2/src/main/java/TournamentOptions.java
+++ challonge-api-v2/src/main/java/TournamentOptions.java
@@ -5,8 +5,6 @@ public abstract class TournamentOptions {
     private final TournamentType tournamentType;
     private final ImmutableMap<String, Object> options;
 
-    //TODO override .equals()
-
     TournamentOptions(String key, TournamentType tournamentType, ImmutableMap<String, Object> options) {
         this.key = key;
         this.tournamentType = tournamentType;
@@ -23,5 +21,29 @@ public abstract class TournamentOptions {
 
     ImmutableMap<String, Object> getOptions() {
         return this.options;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof TournamentOptions)) {
+            return false;
+        }
+        TournamentOptions to = (TournamentOptions)o;
+        if (this.tournamentType != to.tournamentType) {
+            return false;
+        }
+        for (String key : this.options.keySet()) {
+            Object myValue = this.options.get(key);
+            Object toValue = to.options.get(key);
+            if (myValue == null || toValue == null) {
+                if (myValue != toValue) {
+                    return false;
+                }
+            }
+            else if (!myValue.equals(toValue)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
