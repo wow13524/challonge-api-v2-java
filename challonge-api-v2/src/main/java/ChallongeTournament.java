38a6bcb9519d97919fb64115ca60472ca3128514
wow13524 <50389483+wow13524@users.noreply.github.com>
2023-03-28 22:18:48 -0700

    Update ChallongeTournament.java

--- challonge-api-v2/src/main/java/ChallongeTournament.java
+++ challonge-api-v2/src/main/java/ChallongeTournament.java
@@ -69,6 +69,9 @@ public class ChallongeTournament extends ChallongeObject {
 
         this.tournamentOptions =
         ChallongeTournament.parseTournamentOptions(attributes, tournamentType);
+
+        JSONObject timestamps =
+        TypeUtils.requireType(attributes, "timestamps", JSONObject.class);
     }
 
     @SuppressWarnings("unchecked")
